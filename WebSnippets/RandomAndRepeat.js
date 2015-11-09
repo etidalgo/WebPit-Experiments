147121c5cff7e5599546c75ab3ce115a082144ec
etidalgo <etidalgo@sbs.net.nz>
2015-11-10 11:31:31 +1300

    Sample set random tick boxes

--- WebSnippets/RandomAndRepeat.js
+++ WebSnippets/RandomAndRepeat.js
@@ -1,15 +1,11 @@
 
 
 var getRandomInt = function (min, max) {
-
   return Math.floor(Math.random() * (max - min)) + min;
-
 };
 
 var repeatFx = function (fn, times) {
-
     for(var i = 0; i < times; i++)fn();
-
 }
 
 var getRandomInt2 = function() { return getRandomInt(1, 10); };
