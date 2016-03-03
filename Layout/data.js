70d205db13f1fbd0f30998f3e223960248f72f27
etidalgo <etidalgo@sbs.net.nz>
2016-03-03 15:59:25 +1300

    Raw Data

---
+++ Layout/data.js
@@ -0,0 +1,38 @@
+var rulers = [
+{
+	name: "Augustus", 
+	reign: "19BC-14"
+}, 
+{
+	name: "Tiberius", 
+	reign: "14-37"
+}, 
+{
+	name: "Caligula", 
+	reign: "37-41"
+}, 
+{
+	name: "Claudius", 
+	reign: "41-54"
+}, 
+{
+	name: "Nero", 
+	reign: "54-68"
+}, 
+{
+	name: "Galba", 
+	reign: "68-69"
+}, 
+{
+	name: "Otho", 
+	reign: "69-69"
+}, 
+{
+	name: "Vitellius", 
+	reign: "69-69"
+}, 
+{
+	name: "Vespasian", 
+	reign: "69-79"
+}
+];
