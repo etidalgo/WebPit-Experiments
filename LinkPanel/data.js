b3b5ac896312581461adfccffb3877c6a1f6336f
E Tidalgo <etidalgo@gmail.com>
2016-11-17 00:08:14 +1300

    Divide LinkPanel among separate files

---
+++ LinkPanel/data.js
@@ -0,0 +1,20 @@
+var tiPathMap = { 
+	'Patient Demographics': '', 
+	'Appointment Book': 'AppointmentBook.html?bookname=Appointments', 
+	'Configuration': 'admin/Configuration.html', 
+	'Manager': 'admin/TitaniumManager.html',
+	'Academic Management': 'AcademicManagement.html',
+	'Common Tests': 'Spark/Common/tests'
+};		
+var tiWebBase = {
+	TiWebTrunk : 'http://localhost/Titanium/web', 
+	WebDemoNightly : 'https://tiwebdemonightly.isparkdt.com/Titanium/web',
+	WebDemo1 : 'https://tiwebdemo1.isparkdt.com/Titanium/web',
+	WebDemo2 : 'https://tiwebdemo2.isparkdt.com/Titanium/web',
+	'WebDemo1 PHI': 'https://tiwebdemonightly/Titanium_PHI/web',
+	'Local TiWeb 104.12': 'http://localhost/tiweb10412/web',
+	'Local TiWeb 104.14': 'http://localhost/tiweb10414/web',
+	'Local TiWeb 104.16': 'http://localhost/tiweb10416/web',
+	'CRMigrateIsoh': 'http://crmigrateisoh.isparkdt.com/Titanium/web',
+	'Run test debug': 'http://127.0.0.1:9781/Titanium'
+};		
